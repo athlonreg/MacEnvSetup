d85acff63491179e724d4c44486ed96fe72cdab7
athlonreg <15563836030@163.com>
2018-04-16 16:54:56 +0800

    Update macenvconf.sh

--- macenvconf.sh
+++ macenvconf.sh
@@ -47,22 +47,3 @@ brew cask install iTerm2
 # 安装oh-my-zsh
 echo "安装oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# 设置zsh为你的默认shell
-echo "设置zsh为你的默认shell"
-chsh -s /bin/zsh
-
-# 安装tmux
-echo "安装tmux"
-brew install tmux
-
-# 使用gpakoz的tmux配置
-echo "使用gpakoz的tmux配置"
-cd ; rm -rf .tmux
-git clone https://github.com/gpakosz/.tmux.git
-ln -s .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
-
-# 进入tmux开始使用
-tmux 
-echo "欢迎使用tmux"
